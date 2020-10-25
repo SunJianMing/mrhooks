07f1a8997549800980a79de24704b144ffb45f8c
Mr.sun <18273965210@163.com>
2020-10-25 21:59:27 +0800

    update: backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.get('/api/list',(req,res)=>{
             name:'sjm',
             age:29
         },{
-            name:'j',
+            name:'jk',
             age:30
         },{
             name:'m',
